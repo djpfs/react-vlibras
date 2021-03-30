49cefe782f2c879c6c21572f28366729ef208951
João Pedro <jpfs@icomp.ufam.edu.br>
2021-03-30 16:29:24 -0400

    add lib

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+export default class VLibras extends Component {
+    widgetSrc: string;
+    scriptSrc: string;
+    script: HTMLScriptElement;
+    constructor(props: any);
+    init(): void;
+    componentDidMount(): void;
+    render(): string;
+}
